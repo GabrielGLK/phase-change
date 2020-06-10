60519d15faa0165efb3bdd59af8973b6c1d92c0a
GabrielGLK <glk1206@hotmail.com>
2020-06-10 13:33:28 +0200

    add my diffusion method

--- phase-change/phase_change_models/test_cases/stefan-problem/phase_change_code/mydiffusion.h
+++ phase-change/phase_change_models/test_cases/stefan-problem/phase_change_code/mydiffusion.h
@@ -1,110 +1,56 @@
-/**
-## A diffusion-equation solver
+vector hh[];
+void flux_diffusion (scalar c, (const) face vector kappa, face vector F, scalar dc) {
 
-For a species-field concentration $c$, the diffusion equations reads
-(or a version thereof),
-
-$$\frac{\partial c}{\partial t} = \nabla \cdot \left( \kappa \nabla c
-\right).$$
-
-With $\kappa$ the diffusivity of the medium. Because of reasons, we
-wish to use a finite-volume discretization to solve for the temporal
-evolution of $c(\mathbf{x},t)$. We recognize the flux vector ($\mathbf{F}$):
-
-$$\frac{\partial c}{\partial t} + \nabla \cdot \mathbf{F} = 0,$$
-
-as,
-
-$$\mathbf{F} = -\kappa \nabla c.$$
-
-Given the alligment of the faces, we can make a function that writes
-the fluxes ($F$) of $c$ in a face vector field. We also request its
-user to provide the diffusivity on faces, which could be `const`ant:
-*/
-
-void flux_diffusion (scalar c, (const) face vector kappa, face vector F) {
-  /**
- Neighboring cells are possibly behind a boundary of some
-     sort. As such, we need to compute the relevant solution values. 
-*/
   boundary ({c});
-  /**
-By convention, a face separates a cell from its left, bottom, front
-neighbor in the `x,y` and `z` direction, respectively. The
-`foreach_face()` function will automatically rotate over all
-dimensions.
-   */
+
+  foreach()
+    f[] = clamp(f[], 0., 1.);
+  boundary ({f});
+  heights (f, hh);
+  
   foreach_face()
-    F.x[] = -kappa.x[]*(c[] - c[-1])/Delta; //2nd order accurate gradient *estimation*
-}
-
-/**
-   Using this function we can easily compute the tendency for $c$ in a
-   cell ($j$) with $N$ faces:
-
-$$   \left(\frac{\partial c}{\partial t}\right)_j = \frac{1}{V_j} \sum_{i=1}^N \mathbf{F_i}
-   \cdot \mathbf{A_i}.$$
-
-Assuming that the ratio $\frac{V_j}{A_i}=\Delta$. Which is true for
-one-dimensional, square or cubic cells.
-*/
-
-void tendency_from_flux (face vector F, scalar dc) {
+    { 
+      clamp(height(hh.x[]),1e-3,1);
+      if(f[]>=0.5)
+        {        
+          F.x[1] = kappa.x[1]*(c.tr_eq - c[])/(Delta*height(hh.x[])); //2nd order accurate gradient *estimation*
+          F.x[] = kappa.x[]*(c[] - c[-1])/Delta; //2nd order accurate gradient *estimation*
+        }
+      else if(f[]<0.5)
+      {
+          F.x[1] = kappa.x[1]*(c[] - c.tr_eq)/(Delta*height(hh.x[])); //2nd order accurate gradient *estimation*
+          F.x[] = kappa.x[]*(c[] - c[-1])/Delta; //2nd order accurate gradient *estimation*
+        }
+    }
   boundary_flux ({F});  //flux on levels
   foreach() {
     dc[] = 0;
     foreach_dimension() //Rotates over the dimensions
-      dc[] +=  (F.x[] - F.x[1])/Delta; 
+      dc[] +=  (F.x[1] - F.x[])/(0.5*(height(hh.x[]) + Delta)); 
   }
 }
-
-/**
-   With the tendency, the solution can be advanced in time with
-   timestep `dt`.
-*/
 
 void advance (scalar c, scalar dc, double dt, scalar r) {
   foreach()
     c[] += dt*(dc[] + r[]);
 }
 
-/**
-Now we can construct a user-interface function, `diffusion()` that
-employs a fordward-Euler scheme.
- */
 
 void diffusion_1 (scalar c, double dt, face vector kappa, scalar r) {
   face vector F[];
   scalar dc[];
-  flux_diffusion (c, kappa, F);
-  tendency_from_flux (F, dc);
+  flux_diffusion (c, kappa, F, dc);
   advance (c, dc, dt,r);
 }
-/**
-   At the cost of more computational effort and memory, we could also
-   choose to use the mid-point rule to update the solution. It is
-   second-order accurate in time!
- */
+
 
 void diffusion_midpoint (scalar c, double dt, face vector kappa, scalar r) {
   face vector F[];
   scalar dc[], c_temp[];
   foreach()
     c_temp[] = c[];                 //create a scratch
-  flux_diffusion(c_temp, kappa, F); 
-  tendency_from_flux (F, dc);
+  flux_diffusion(c_temp, kappa, F, dc); 
   advance (c_temp, dc, dt/2.,r);     //advance to the mid point.
-  flux_diffusion(c_temp, kappa, F); //Re-estimate the fluxes at the mid point,
-  tendency_from_flux (F, dc);       //and the tendency there.
+  flux_diffusion(c_temp, kappa, F, dc); //Re-estimate the fluxes at the mid point,
   advance (c, dc, dt,r);             //update the solution
 }
-
-/**
-# Further, 
-
-We could use the [`Runke-Kutta`](/src/runge-kutta.h) schemes (upto 4th
-order) or a [predictor corrector](/src/predictor-corrector.h) scheme.
- */
-
-
-
